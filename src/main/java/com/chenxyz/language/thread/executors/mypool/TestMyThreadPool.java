36985bd14d4eb558774966738f72d297c13c8236
chenxyz <chenxyz@163.com>
2018-08-10 15:47:39 +0800

    ScheduledPool CompletionService & Future

--- src/main/java/com/chenxyz/language/thread/executors/mypool/TestMyThreadPool.java
+++ src/main/java/com/chenxyz/language/thread/executors/mypool/TestMyThreadPool.java
@@ -1,10 +1,9 @@
 package com.chenxyz.language.thread.executors.mypool;
 
 import java.util.Random;
-import java.util.concurrent.CountDownLatch;
 
 /**
- * Description
+ * 查看线程id 被复用
  *
  * @author chenxyz
  * @version 1.0
@@ -12,18 +11,16 @@ import java.util.concurrent.CountDownLatch;
  */
 public class TestMyThreadPool {
     public static void main(String[] args) throws InterruptedException {
-        CountDownLatch cdl = new CountDownLatch(1);
         // 创建3个线程的线程池
         MyThreadPool t = new MyThreadPool(3);
-        t.execute(new MyTask("task1", cdl));
-        t.execute(new MyTask("task2", cdl));
-        t.execute(new MyTask("task3", cdl));
-        t.execute(new MyTask("task4", cdl));
-        t.execute(new MyTask("task5", cdl));
+        t.execute(new MyTask("task1"));
+        t.execute(new MyTask("task2"));
+        t.execute(new MyTask("task3"));
+        t.execute(new MyTask("task4"));
+        t.execute(new MyTask("task5"));
 
-//        Thread.sleep(1000);
+        Thread.sleep(1000);
         System.out.println(t);
-        cdl.countDown();
         Thread.sleep(90000);
         t.destroy();// 所有线程都执行完成才destory
         System.out.println(t);
@@ -35,28 +32,19 @@ public class TestMyThreadPool {
 
         private String name;
         private Random r = new Random();
-        private CountDownLatch cdl;
 
-        public MyTask(String name, CountDownLatch cdl) {
+        public MyTask(String name) {
             this.name = name;
+            System.out.println("my task 初始化完成！！");
         }
 
         public String getName() {
             return name;
         }
 
-        public CountDownLatch getCdl() {
-            return cdl;
-        }
-
-        public void setCdl(CountDownLatch cdl) {
-            this.cdl = cdl;
-        }
-
         @Override
         public void run() {// 执行任务
             try {
-                cdl.await();
                 Thread.sleep(r.nextInt(1000)+2000);
                 System.out.println(Thread.currentThread().getId()+" is executing!!!");
             } catch (InterruptedException e) {
