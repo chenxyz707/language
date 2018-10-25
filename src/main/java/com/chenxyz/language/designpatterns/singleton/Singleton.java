82b20bbded51cac2e26f095eddc86637d72e9956
chenxyz707 <chenxyz@163.com>
2018-10-25 22:52:25 +0800

    singleton some errors

--- src/main/java/com/chenxyz/language/designpatterns/singleton/Singleton.java
+++ src/main/java/com/chenxyz/language/designpatterns/singleton/Singleton.java
@@ -10,12 +10,18 @@ package com.chenxyz.language.designpatterns.singleton;
  */
 public class Singleton {
 
-    private static Singleton singleton;
+    private static volatile Singleton singleton;
+
+    //私有构造器
+    private Singleton() {}
 
     public static Singleton getInstance() {
         if(singleton!=null) {
             synchronized (Singleton.class) {
                 if(singleton!=null) {
+                    // 其实这一步也很难以保证它真的是单例的，因为创建对象和赋值是两个操作，
+                    // singleton可能还没有指向Singleton这个实例，这时为空
+                    // 其他线程也是可以进来的
                     singleton = new Singleton();
                 }
             }
